2e708c0089ee4137fc922da5a47fcf05bf1c7ba2
Holly Schilling <holly.a.schilling@outlook.com>
2017-01-31 21:21:25 -0600

    Fixed broken links for dependencies.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 var package = Package(
     name: "BetterSQLite",
     dependencies: [
-        .Package(url: "../BetterLibrary", majorVersion: 1),
-        .Package(url: "../CSQLite3", majorVersion: 1),
+        .Package(url: "https://github.com/BetterPractice/BetterLibrary.git", majorVersion: 1),
+        .Package(url: "https://github.com/BetterPractice/CSQLite3.git", majorVersion: 1),
     ]
 )
